8b1f940030be304bf9de4c58f16ba9dcb9a6cd73
jiabailie <spotlightyrg@gmail.com>
2013-06-22 09:49:28 +0800

    Update setInterection.cpp

--- setInterection.cpp
+++ setInterection.cpp
@@ -2,31 +2,35 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <functional>
 
 using namespace std;
 
 void GetIntersection( const vector<int>& vectorA, const vector<int>& vectorB, vector<int>& vectorC )
 {
-  int i = 0;
+	int i = 0;
 	int sta = 0;
 	int end = 0;
 	int mid = 0;
-	bool f = false;
+	bool flg = false;
 	int la = (int)vectorA.size();
 	int lb = (int)vectorB.size();
-	vector<int> tmp;
+	vector<int> tmp(vectorA);
 
-	for(i = 0; i < la; i++)
+	/* clear vectorC's content */
+	if(!vectorC.empty())
 	{
-		tmp.push_back(vectorA[i]);
+		vectorC.clear();
 	}
+
+	/* sort the elements in tmp */
 	sort(tmp.begin(), tmp.end());
+
+	/* using binary search to find if vectorB[i] is also in vectorA */
 	for(i = 0; i < lb; i++)
 	{
 		if(vectorB[i] >= tmp[0] && vectorB[i] <= tmp[la - 1])
 		{
-			f = false;
+			flg = false;
 			sta = 0;
 			end = la - 1;
 			mid = sta + end / 2;
@@ -34,25 +38,25 @@ void GetIntersection( const vector<int>& vectorA, const vector<int>& vectorB, ve
 			{
 				if(vectorB[i] == tmp[mid])
 				{
-					f = true;
+					flg = true;
 					break;
 				}
 				if(vectorB[i] > tmp[mid])
 				{
 					sta = mid + 1;
-					mid = sta + (end - sta) / 2;
 				}
 				if(vectorB[i] < tmp[mid])
 				{
 					end = mid - 1;
-					mid = sta + (end - sta) / 2;
 				}
 				if(sta > end)
 				{
 					break;
 				}
+				mid = sta + (end - sta) / 2;
 			}
-			if(f)
+			/* if vectorB[i] is being found in tmp */
+			if(flg)
 			{
 				vectorC.push_back(vectorB[i]);
 			}
@@ -66,11 +70,18 @@ int main()
 	a.push_back(3);
 	a.push_back(2);
 	a.push_back(1);
+	a.push_back(56);
 	b.push_back(2);
 	b.push_back(3);
-	b.push_back(5);
+	b.push_back(56);
+	c.push_back(19);
 
 	GetIntersection(a, b, c);
+
+	for(vector<int>::iterator it = c.begin(); it != c.end(); it++)
+	{
+		cout << (*it) << " ";
+	}
 
 	return 0;
 }
