8965e3d59dea14967fff4dedb000bcfa29a95b9f
Ruiguo Yang <spotlightyrg@gmail.com>
2013-12-05 11:12:25 +0800

    Update rename.bat

--- cmd/rename.bat
+++ cmd/rename.bat
@@ -1,4 +1,9 @@
 @echo off
+
+: create 36 file folder, using %%i as the folder name
 for /l %%i in (0, 1, 35) do md %%i
+
+: rename all file folders under this directory, but you should input the new name one by one
 for /d %%i in (dir *) do ren %%i [the name you want]
+
 @echo on
