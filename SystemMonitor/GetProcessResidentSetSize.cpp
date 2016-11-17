8ca816aa4f26f6ff667cf5cfebf536ca56cff2ef
Ruiguo Yang <spotlightyrg@gmail.com>
2016-11-17 13:49:57 +0800

    Add usage samples.

--- SystemMonitor/GetProcessResidentSetSize.cpp
+++ SystemMonitor/GetProcessResidentSetSize.cpp
@@ -5,6 +5,9 @@
  *          http://creativecommons.org/licenses/by/3.0/deed.en_US
  * Reference: 
  *          http://nadeausoftware.com/articles/2012/07/c_c_tip_how_get_process_resident_set_size_physical_memory_use
+ * Usage:
+ *          size_t currentSize = getCurrentRSS();
+ *          size_t peakSize    = getPeakRSS();
  */
 
 #if defined(_WIN32)
